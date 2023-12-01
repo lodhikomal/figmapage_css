d33dfbdb40bb470e6022e1065a778bc9b375b67e
lodhikomal <komallodhi720@gmail.com>
2023-12-01 12:38:22 +0530

    hh

--- src/component/Wrapper.js
+++ src/component/Wrapper.js
@@ -8,7 +8,7 @@ function Wrapper(props) {
         <img src={image} alt="" className="wrap-image" style={style}></img>
       </div>
       <div className="wrapprer-text">
-        <h4 className="wrapper-heading {props.newClass}">{heading}</h4>
+        <h4 className={`wrapper-heading ${props.newClass}`}>{heading}</h4>
         {SubTitleComp ? <SubTitleComp /> : props.p}
         {showButton && <button className="wrapper-button">{buttonText}</button>}
       </div>
